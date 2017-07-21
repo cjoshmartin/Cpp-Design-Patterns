15392b8ec2a48ebacb6e8e7d0f460d3230011c3b
Josh Martin <joshmartin5001@gmail.com>
2017-07-20 23:17:19 -0400

    the wrong way of Liskov Substitution Principle

--- creational/code_samples/LSP.cpp
+++ creational/code_samples/LSP.cpp
@@ -36,18 +36,36 @@ class Rectangle
     }
     int  Area() const { return width * height;}
 
+
+};
+
+class Square : public Rectangle
+{
+public:
+    Square(int size) : Rectangle{size,size{
+    };
+    void setWidth(const int width) override {
+     this->width=height = width;
+    };
+    void setHeight(const int height) override{
+        this->height= width = height;
+    };
+
+
+};
     void process(Rectangle& r){
        int w = r.getWidth();
         r.setHeight(10);
         std::cout<< "expect Area =" << (w * 10) << ", got " << r.Area() << std::endl;
     }
-};
-
 
 int main(){
 
     Rectangle r{5,5};
     process(r);
+
+    Square s{ 5 };
+    process(s);
 
     getchar();
     return 0;
