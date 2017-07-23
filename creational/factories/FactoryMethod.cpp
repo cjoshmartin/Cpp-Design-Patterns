a78d504f35955300495ee653b43c34762c6b511c
Josh Martin <joshmartin5001@gmail.com>
2017-07-22 20:31:31 -0400

    basic factories

--- creational/factories/FactoryMethod.cpp
+++ creational/factories/FactoryMethod.cpp
@@ -4,28 +4,48 @@
 #include <iostream>
 
 
-enum class PointType
-{
-    cartesian,
-    polar
-};
 
 class Point
 {
+
+    Point(const float a, const float b)
+            : x{a},
+              y{b}
+    {
+    }
 public:
     float x,y;
 
-    Point(const float a, const float b, PointType type = PointType::cartesian)
+    // beginning of Factories
+    static Point NewCartesian(const float x, const float y)
     {
-        if(type == PointType::cartesian)
-        {
-            x = a;
-            y = b;
-        }
-        else
-        {
-            x = a * cos(b);
-            y = a=a * sin(b);
-        }
+        return {x,y};
+    }
+
+    static Point NewPolar(const float r, const float theta)
+    {
+        return {r*cos(theta),r*sin(theta)};
+    }
+
+    // end of Factories
+    friend std::ostream& operator<<(std::ostream& os, const Point& obj)
+    {
+        return os << "x: " << obj.x << " y: " << obj.y;
     }
 };
+
+
+
+int main()
+{
+    Point p{1,2};
+    // Cartesian
+    auto c = Point::NewCartesian(1,2);
+    std::cout << c << std::endl;
+
+    // Polar
+    std::cout << Point::NewPolar(5,M_PI_4) << std::endl;
+
+    getchar();
+    return 0;
+}
