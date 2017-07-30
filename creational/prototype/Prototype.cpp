45b2afe50e34dfd53c6aad89c5f1e303f4c5ef53
Josh Martin <joshmartin5001@gmail.com>
2017-07-30 11:12:27 -0400

    life with out using prototypes

--- creational/prototype/Prototype.cpp
+++ creational/prototype/Prototype.cpp
@@ -5,18 +5,48 @@ using namespace std;
 
 struct Address {
 	string street;
-	string city; 
-}
+	string city;
+	int suite;
+
+
+	friend std::ostream& operator<<(std::ostream& os, const Address& obj)
+	{
+		return os
+			<< "street: " << obj.street
+			<< " city: "  << obj.city
+			<< " suite: " << obj.suite;
+	}
+};
 
 struct Contact{
 	string name;
 	Address* work_address;
+	~Contact(){
+		//delete work_address;
+	}
 
-}
+	friend std::ostream& operator<<(std::ostream& os, const Contact& obj)
+	{
+		return os
+			<< "name: " 		 << obj.name
+			<< " work_address: " << *obj.work_address;
+	}
+};
 
 int main()
 {
-Contact john{"John Doe", new Address{"123 East dr",}}
-getchar();
-return 0;
+    Address* addr = new Address{"123 East Dr", "London" }; // want to prototype
+
+	Contact john{"John Doe",addr};
+	john.work_address->suite=100;
+
+	Contact jane{"Jane Doe",addr};
+	jane.work_address->suite=123;
+
+	cout << john << endl << jane <<endl; // print john and jane to have the same suit address ( that is not what we want )
+
+	delete addr;
+
+	getchar();
+	return 0;
 }
