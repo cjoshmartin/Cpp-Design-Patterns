9cc972f8decb1e2b56130c30fe2ff6dea21d695c
Josh Martin <joshmartin5001@gmail.com>
2017-07-30 11:45:37 -0400

    deep copy for jane and john

--- creational/prototype/Prototype.cpp
+++ creational/prototype/Prototype.cpp
@@ -24,14 +24,14 @@ struct Contact{
 
 	Contact(const string& name, Address* const work_address) // this construct is so that John and Jane still work.
 			: name{name},
-			  work_address{work_address} {}
+			  work_address{new Address{*work_address}} {}
 
 	Contact(const Contact& other) // this construct is how Jill works
 			: name{other.name},
 			work_address(new Address{*other.work_address}) {}
 
 	~Contact(){
-		//delete work_address;
+		delete work_address;
 	}
 
 	friend std::ostream& operator<<(std::ostream& os, const Contact& obj)
@@ -52,10 +52,7 @@ int main()
 	Contact jane{"Jane Doe",addr};
 	jane.work_address->suite=123;
 
-	cout << john << endl << jane <<endl;   // print john and jane to have the same suit address ( that is not what we want ).
-										  //  The problem comes from that we are shallow copying all the everything
-										 //   which means, copies the address of pointers not the value of said pointer
-										//	  ==> john.work_address <===> jane.work_address
+	cout << john << endl << jane <<endl;  // it works great now!
 	Contact jill { jane };
 	jill.work_address->suite = 1000;
 	cout << jill << endl; 				//   However this will work because of the new constructor have added
